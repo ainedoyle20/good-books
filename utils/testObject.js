8d35e279a56ac066ec6bb03efa0f3a7d09864326
Aine Doyle <ainedoyleAD@gmail.com>
2022-10-08 13:32:08 +0100

    initializing repository, installed required packages

---
+++ utils/testObject.js
@@ -0,0 +1,365 @@
+const BookLists = [
+  {
+    "id": 1,
+    "name": "Best Books Ever",
+    "url": "https://www.goodreads.com/list/show/1.Best_Books_Ever",
+    "booksAmount": 54920,
+    "voters": 217693,
+    "preview": [
+      {
+        "title": "The Hunger Games by Suzanne Collins",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1586722975l/2767052._SX98_.jpg"
+      },
+      {
+        "title": "Harry Potter and the Order of the Phoenix by J.K. Rowling",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1546910265l/2._SX98_.jpg"
+      },
+      {
+        "title": "To Kill a Mockingbird by Harper Lee",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1553383690l/2657._SX98_.jpg"
+      },
+      {
+        "title": "Pride and Prejudice by Jane Austen",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1320399351l/1885._SX98_.jpg"
+      },
+      {
+        "title": "Twilight by Stephenie Meyer",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1361039443l/41865._SX98_.jpg"
+      }
+    ]
+  },
+  {
+    "id": 264,
+    "name": "Books That Everyone Should Read At Least Once",
+    "url": "https://www.goodreads.com/list/show/264.Books_That_Everyone_Should_Read_At_Least_Once",
+    "booksAmount": 24210,
+    "voters": 103542,
+    "preview": [
+      {
+        "title": "To Kill a Mockingbird by Harper Lee",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1553383690l/2657._SX98_.jpg"
+      },
+      {
+        "title": "Harry Potter and the Sorcerer's Stone by J.K. Rowling",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1474154022l/3._SX98_.jpg"
+      },
+      {
+        "title": "Pride and Prejudice by Jane Austen",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1320399351l/1885._SX98_.jpg"
+      },
+      {
+        "title": "The Diary of a Young Girl by Anne Frank",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1560816565l/48855._SY160_.jpg"
+      },
+      {
+        "title": "Animal Farm by George Orwell",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1325861570l/170448._SY160_.jpg"
+      }
+    ]
+  },
+  {
+    "id": 43,
+    "name": "Best Young Adult Books",
+    "url": "https://www.goodreads.com/list/show/43.Best_Young_Adult_Books",
+    "booksAmount": 11757,
+    "voters": 82273,
+    "preview": [
+      {
+        "title": "Harry Potter and the Sorcerer's Stone by J.K. Rowling",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1474154022l/3._SX98_.jpg"
+      },
+      {
+        "title": "The Lightning Thief by Rick Riordan",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1400602609l/28187._SX98_.jpg"
+      },
+      {
+        "title": "The Hunger Games by Suzanne Collins",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1586722975l/2767052._SX98_.jpg"
+      },
+      {
+        "title": "Twilight by Stephenie Meyer",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1361039443l/41865._SX98_.jpg"
+      },
+      {
+        "title": "The Giver by Lois Lowry",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1342493368l/3636._SY160_.jpg"
+      }
+    ]
+  },
+  {
+    "id": 1043,
+    "name": "Books That Should Be Made Into Movies",
+    "url": "https://www.goodreads.com/list/show/1043.Books_That_Should_Be_Made_Into_Movies",
+    "booksAmount": 28907,
+    "voters": 70507,
+    "preview": [
+      {
+        "title": "City of Ashes by Cassandra Clare",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1432730356l/1582996._SX98_.jpg"
+      },
+      {
+        "title": "Hush, Hush by Becca Fitzpatrick",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1358261334l/6339664._SX98_.jpg"
+      },
+      {
+        "title": "City of Glass by Cassandra Clare",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1369452339l/3777732._SX98_.jpg"
+      },
+      {
+        "title": "Clockwork Angel by Cassandra Clare",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1454962884l/7171637._SX98_.jpg"
+      },
+      {
+        "title": "Marked by P.C. Cast",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1317067002l/30183._SX98_.jpg"
+      }
+    ]
+  },
+  {
+    "id": 6,
+    "name": "Best Books of the 20th Century",
+    "url": "https://www.goodreads.com/list/show/6.Best_Books_of_the_20th_Century",
+    "booksAmount": 7554,
+    "voters": 49995,
+    "preview": [
+      {
+        "title": "To Kill a Mockingbird by Harper Lee",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1553383690l/2657._SX98_.jpg"
+      },
+      {
+        "title": "1984 by George Orwell",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1532714506l/40961427._SX98_.jpg"
+      },
+      {
+        "title": "Harry Potter and the Sorcerer's Stone by J.K. Rowling",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1474154022l/3._SX98_.jpg"
+      },
+      {
+        "title": "The Great Gatsby by F. Scott Fitzgerald",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1490528560l/4671._SX98_.jpg"
+      },
+      {
+        "title": "Animal Farm by George Orwell",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1325861570l/170448._SY160_.jpg"
+      }
+    ]
+  },
+  {
+    "id": 5,
+    "name": "Best Books of the Decade: 2000s",
+    "url": "https://www.goodreads.com/list/show/5.Best_Books_of_the_Decade_2000s",
+    "booksAmount": 6871,
+    "voters": 28359,
+    "preview": [
+      {
+        "title": "Harry Potter and the Deathly Hallows by J.K. Rowling",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1474171184l/136251._SX98_.jpg"
+      },
+      {
+        "title": "The Hunger Games by Suzanne Collins",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1586722975l/2767052._SX98_.jpg"
+      },
+      {
+        "title": "Harry Potter and the Half-Blood Prince by J.K. Rowling",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1587697303l/1._SX98_.jpg"
+      },
+      {
+        "title": "Harry Potter and the Goblet of Fire by J.K. Rowling",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1554006152l/6._SX98_.jpg"
+      },
+      {
+        "title": "Harry Potter and the Order of the Phoenix by J.K. Rowling",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1546910265l/2._SX98_.jpg"
+      }
+    ]
+  },
+  {
+    "id": 10762,
+    "name": "Best Book Boyfriends",
+    "url": "https://www.goodreads.com/list/show/10762.Best_Book_Boyfriends",
+    "booksAmount": 9369,
+    "voters": 28129,
+    "preview": [
+      {
+        "title": "Beautiful Disaster by Jamie McGuire",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1358259032l/11505797._SX98_.jpg"
+      },
+      {
+        "title": "Fifty Shades of Grey by E.L. James",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1385207843l/10818853._SX98_.jpg"
+      },
+      {
+        "title": "Hopeless by Colleen Hoover",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1353489892l/15717943._SX98_.jpg"
+      },
+      {
+        "title": "The Fault in Our Stars by John Green",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1632632557l/11870085._SX98_.jpg"
+      },
+      {
+        "title": "Divergent by Veronica Roth",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1618526890l/13335037._SX98_.jpg"
+      }
+    ]
+  },
+  {
+    "id": 15,
+    "name": "Best Historical Fiction",
+    "url": "https://www.goodreads.com/list/show/15.Best_Historical_Fiction",
+    "booksAmount": 6719,
+    "voters": 26527,
+    "preview": [
+      {
+        "title": "Memoirs of a Geisha by Arthur Golden",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1409595968l/929._SX98_.jpg"
+      },
+      {
+        "title": "Gone with the Wind by Margaret Mitchell",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1551144577l/18405._SX98_.jpg"
+      },
+      {
+        "title": "The Pillars of the Earth by Ken Follett",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1576956100l/5043._SX98_.jpg"
+      },
+      {
+        "title": "The Book Thief by Markus Zusak",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1522157426l/19063._SX98_.jpg"
+      },
+      {
+        "title": "The Other Boleyn Girl by Philippa Gregory",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1355932638l/37470._SX98_.jpg"
+      }
+    ]
+  },
+  {
+    "id": 47,
+    "name": "Best Dystopian and Post-Apocalyptic Fiction",
+    "url": "https://www.goodreads.com/list/show/47.Best_Dystopian_and_Post_Apocalyptic_Fiction",
+    "booksAmount": 3612,
+    "voters": 25653,
+    "preview": [
+      {
+        "title": "The Hunger Games by Suzanne Collins",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1586722975l/2767052._SX98_.jpg"
+      },
+      {
+        "title": "1984 by George Orwell",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1532714506l/40961427._SX98_.jpg"
+      },
+      {
+        "title": "The Giver by Lois Lowry",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1342493368l/3636._SY160_.jpg"
+      },
+      {
+        "title": "Divergent by Veronica Roth",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1618526890l/13335037._SX98_.jpg"
+      },
+      {
+        "title": "Brave New World by Aldous Huxley",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1575509280l/5129._SX98_.jpg"
+      }
+    ]
+  },
+  {
+    "id": 50,
+    "name": "The Best Epic Fantasy (fiction)",
+    "url": "https://www.goodreads.com/list/show/50.The_Best_Epic_Fantasy_fiction_",
+    "booksAmount": 3504,
+    "voters": 25064,
+    "preview": [
+      {
+        "title": "A Game of Thrones by George R.R. Martin",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1562726234l/13496._SY160_.jpg"
+      },
+      {
+        "title": "J.R.R. Tolkien 4-Book Boxed Set by J.R.R. Tolkien",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1346072396l/30._SX98_.jpg"
+      },
+      {
+        "title": "The Name of the Wind by Patrick Rothfuss",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1270352123l/186074._SX98_.jpg"
+      },
+      {
+        "title": "The Way of Kings by Brandon Sanderson",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1388184640l/7235533._SX98_.jpg"
+      },
+      {
+        "title": "The Chronicles of Narnia by C.S. Lewis",
+        "imageURL": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1449868701l/11127._SX98_.jpg"
+      }
+    ]
+  }
+]
+
+const SearchResults = [
+  {
+    "book_id": 138398,
+    "name": "The Walking Dead, Vol. 1: Days Gone Bye",
+    "cover": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1389233242i/138398._SY75_.jpg",
+    "url": "https://www.goodreads.com/book/show/138398.The_Walking_Dead_Vol_1?from_search=true&from_srp=true&qid=nK9YZcAHq9&rank=1",
+    "authors": [
+      "Robert Kirkman",
+      "Tony Moore",
+      "Andreas Mergenthaler",
+      "Frank Darabont",
+      "Charles Adlard",
+      "Sina Grace",
+      "Hardy Hellstern",
+      "Charlie Adlard",
+      "Rus Wooton",
+      "Rus Wooton",
+      "Stefano Gaudiano",
+      "Cliff Rathburn",
+      "Marc-Oliver Frisch",
+      "Dave    Stewart",
+      "Michael Schuster",
+      "Frank Neubauer"
+    ],
+    "rating": 4.27,
+    "created_editions": 48,
+    "year": 2004
+  },
+  {
+    "book_id": 6465707,
+    "name": "The Walking Dead Compendium One",
+    "cover": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1449865699i/6465707._SY75_.jpg",
+    "url": "https://www.goodreads.com/book/show/6465707-the-walking-dead-compendium-one?from_search=true&from_srp=true&qid=nK9YZcAHq9&rank=2",
+    "authors": [
+      "Robert Kirkman"
+    ],
+    "rating": 4.44,
+    "created_editions": 13,
+    "year": 2009
+  },
+  {
+    "book_id": 30071,
+    "name": "The Walking Dead, Book One",
+    "cover": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1464733555i/30071._SY75_.jpg",
+    "url": "https://www.goodreads.com/book/show/30071.The_Walking_Dead_Book_One?from_search=true&from_srp=true&qid=nK9YZcAHq9&rank=3",
+    "authors": [
+      "Robert Kirkman",
+      "Tony Moore",
+      "Charlie Adlard"
+    ],
+    "rating": 4.35,
+    "created_editions": 14,
+    "year": 2004
+  }
+]
+
+// Only need to store key (book_id) and value (pages, synopsis, published_date)
+const BookDetails = {
+  "book_id": 56597885,
+  "name": "Beautiful World, Where Are You",
+  "cover": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1618329605l/56597885.jpg",
+  "url": "https://www.goodreads.com/book/show/56597885",
+  "authors": [
+    "Sally Rooney"
+  ],
+  "rating": 3,
+  "pages": 356,
+  "published_date": "September 7th 2021",
+  "synopsis": "Beautiful World, Where Are You is a new novel by Sally Rooney, the bestselling author of Normal People and Conversations with Friends.Alice, a novelist, meets Felix, who works in a warehouse, and asks him if he’d like to travel to Rome with her. In Dublin, her best friend, Eileen, is getting over a break-up and slips back into flirting with Simon, a man she has known sinceBeautiful World, Where Are You is a new novel by Sally Rooney, the bestselling author of Normal People and Conversations with Friends.Alice, a novelist, meets Felix, who works in a warehouse, and asks him if he’d like to travel to Rome with her. In Dublin, her best friend, Eileen, is getting over a break-up and slips back into flirting with Simon, a man she has known since childhood. Alice, Felix, Eileen, and Simon are still young—but life is catching up with them. They desire each other, they delude each other, they get together, they break apart. They have sex, they worry about sex, they worry about their friendships and the world they live in. Are they standing in the last lighted room before the darkness, bearing witness to something? Will they find a way to believe in a beautiful world?"
+}
+
+console.log(BookDetails.book_id);
